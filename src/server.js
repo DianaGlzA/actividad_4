7ef0dc20d6b2ac93e068681cc125a03ea29f7559
Diana Gonzalez <dainasld7@gmail.com>
2026-02-13 23:21:22 -0600

    login

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ const path = require('path');
 require('dotenv').config(); 
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const connectDB = require('./config/db')
 const app = express(); 
 
